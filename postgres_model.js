26dc0bff34dc21b7ce80eeb0d0c57e4e3195595e
jas9reetctf <jas9reet.ctf@gmail.com>
2026-03-16 01:46:59 +0530

    Update postgres_model.js

--- postgres_model.js
+++ postgres_model.js
@@ -1,6 +1,6 @@
-var pg_port=1212;
-var pg_host="gitguardians.com:9082/BLUDB";
-var pg_user="root";
-var pg_pass="sup3rstr0ngpass1ForGG";
+var pg_port=12121;
+var pg_host="gitguardians.com:9082/BLUDBs";
+var pg_user="roots";
+var pg_pass="sup3rstr0ngpass1ForGGs";
 
-var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
+var mongo_uri = "mongodb+srv://testusers:hub24aoeu@gg-is-awesome-gg273s.mongodb.net/test?retryWrites=true&w=majority";
